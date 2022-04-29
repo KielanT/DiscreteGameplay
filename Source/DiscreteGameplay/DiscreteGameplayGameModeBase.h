12d89edd341be8ec9333bd16bf98cc464384ac0e
KielanT <34215034+KielanT@users.noreply.github.com>
2022-04-29 19:31:01 +0100

    Initial commit

---
+++ Source/DiscreteGameplay/DiscreteGameplayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DiscreteGameplayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DISCRETEGAMEPLAY_API ADiscreteGameplayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
